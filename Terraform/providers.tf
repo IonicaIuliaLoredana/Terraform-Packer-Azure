a25fdd17a9972e47d3b7d2fde1d1f1bfdd6faf83
ionicaiulialoredana@yahoo.com <IonicaIuliaLoredana>
2023-12-14 13:59:55 +0100

    first commit

---
+++ Terraform/providers.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">=0.12"
+
+  required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = "a7b05ded-7e96-4ce0-ba1c-f5be49918f1d"
+}
